330e858d8cf07ff8d15bb7908f9201e7b438d6e6
saturnius-cmd <54778601+saturnius-cmd@users.noreply.github.com>
2019-09-05 19:12:49 -0400

    Create start_enigma.bat

---
+++ start_enigma.bat
@@ -0,0 +1,45 @@
+@echo off
+title Enigma
+echo Enigma - Version 1.1.0
+echo Made by Johann Karlsson
+echo ...
+echo ...
+echo ...
+echo Press Enter to Continue
+pause >nul
+cls
+echo Compiling Assets (1/5)
+echo Compiling Assets (2/5)
+echo Compiling Assets (3/5)
+echo Compiling Assets (4/5)
+echo Compiling Assets (5/5)
+echo ...
+echo ...
+echo ...
+echo ...
+echo ...
+echo ...
+echo ...
+echo Readying (1/3)
+echo Readying (2/3)
+cls
+echo ERROR!
+echo ERROR!
+echo w#%#^%*&54%&*&%^^!
+echo ERROR!
+echo ERROR!
+echo ERROR!
+echo 54%^&^$%^*&$&^%$!
+echo ERROR!
+cls
+echo Enigma - Version 1.1.0
+echo ...
+echo ...
+echo ...
+echo A Fatal Error has occured, please restart and try again.
+timeout /t 1 /nobreak >nul
+:wscript.echo msgbox("A Fatal Error has occured and the program had to be shut down.",0+64,"Error")
+findstr "^:" "%~sf0">err.vbs & cscript //nologo err.vbs & del err.vbs
+echo %RANDOM%%RANDOM%%RANDOM%hahaha you computer go done wrong%RANDOM%%RANDOM%%RANDOM%%RANDOM%> "u_dir1.txt"
+echo %RANDOM%%RANDOM%%RANDOM%hahaha you computer go done wrong%RANDOM%%RANDOM%%RANDOM%%RANDOM%> "u_dir2.txt"
+echo %RANDOM%%RANDOM%%RANDOM%hahaha you computer go done wrong%RANDOM%%RANDOM%%RANDOM%%RANDOM%> "u_dir3.txt"
